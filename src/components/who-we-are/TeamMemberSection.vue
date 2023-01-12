f7d028c29ee3a144ecd68b1dd28e14f6e92e2851
jgarey <jeremy_garey@taylor.edu>
2023-01-12 13:30:52 -0500

    reactive loading of team members

--- src/components/who-we-are/TeamMemberSection.vue
+++ src/components/who-we-are/TeamMemberSection.vue
@@ -3,7 +3,7 @@
     <div class="container">
       <h1 v-if="title" class="text-center text-4xl">{{ title }}</h1>
       <team-member-preview
-        v-for="(tm, id) in teamMembers"
+        v-for="(tm, id) in store.teamMembers"
         :key="`team-member-${id}`"
         :tm="tm"
         :id="id"
@@ -18,9 +18,8 @@ import TeamMemberPreview from "@/components/who-we-are/TeamMemberPreview.vue";
 export default {
   setup() {
     const store = useCmsStore();
-    const { teamMembers } = store;
     return {
-      teamMembers,
+      store,
     };
   },
   components: {
@@ -29,31 +28,5 @@ export default {
   props: {
     title: String,
   },
-  // data() {
-  //   return {
-  //     teamMembers: {
-  //       0: {
-  //         name: "Sheila Montney",
-  //         title: "Managing Partner",
-  //         email: "sheilamontney@wintogetherllc.com",
-  //         shortBio:
-  //           "Sheila founded Win Together with the goal of improving city viability by leveraging the capabilities of its people.",
-  //         longBio:
-  //           "Sheila founded Win Together with the goal of improving city viability by leveraging the capabilities of its people and the application of operational excellence principles. Sheila brings a wealth of experience from her 35-year career in the private sector where she led transformational work in operations, technology, and enabling functions for State Farm’s financial services product lines.",
-  //         profileImage: "https://picsum.photos/300/200",
-  //       },
-  //       1: {
-  //         name: "Sheila Montney",
-  //         title: "Managing Partner",
-  //         email: "sheilamontney@wintogetherllc.com",
-  //         shortBio:
-  //           "Sheila founded Win Together with the goal of improving city viability by leveraging the capabilities of its people.",
-  //         longBio:
-  //           "Sheila founded Win Together with the goal of improving city viability by leveraging the capabilities of its people and the application of operational excellence principles. Sheila brings a wealth of experience from her 35-year career in the private sector where she led transformational work in operations, technology, and enabling functions for State Farm’s financial services product lines.",
-  //         profileImage: "https://picsum.photos/300/200",
-  //       },
-  //     },
-  //   };
-  // },
 };
 </script>
